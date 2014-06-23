da34a0f6c45b1852656f1b087ff383b3a45949eb
scopevale <scopevale@gmail.com>
2014-06-23 21:06:50 +0000

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+$(document).foundation();
+
+/**
+ * Author: Gary Smith <scopevale@gmail.com>
+ * 
+ * various jQuery events for Navigation
+**/
+(function(){
+	var bgColour;
+
+	$('#navbar a').mouseover(function() {
+		var jQBar = $(this).next('div.menu-item-bottom');
+		bgColour = jQBar.css("background-color");
+		var colour = jQBar.find('div.lozenge').css("background-color");
+		jQBar.css({'background-color' : colour});
+		// $(this).css({'color' : colour});
+	});
+
+	$('#navbar a').mouseout(function() {
+		// $(this).css({'color' : bgColour});
+		$(this).next('div.menu-item-bottom').css({'background-color' : bgColour});
+	});
+
+	$('#disclaimer > h5').click(function() {
+		$('#disclaimer-modal').dialog({
+			height: 320,
+			width: 500,
+			modal: true
+		});
+	});
+
+})();
